c792aa2a1a704c2ae9e0a3cf5f75f6fefa7bcdac
Tomás Alberdi <66546046+TomyAlberdi@users.noreply.github.com>
2025-05-23 18:34:45 -0300

    implemented add/remove favorite functionality in panel

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,23 @@
 import { useDataContext } from "@/Context/useDataContext";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
-import { Search } from "lucide-react";
+import { Search, Sparkle } from "lucide-react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 const SearchBar = () => {
-  const { getWeatherData } = useDataContext();
+  const {
+    getWeatherData,
+    addToFavorites,
+    removeFromFavorites,
+    Favorites,
+    WeatherData,
+  } = useDataContext();
 
   const [city, setCity] = useState<string>("");
 
@@ -32,8 +43,46 @@ const SearchBar = () => {
     setCity("");
   };
 
+  const handleFavorite = () => {
+    const cityName = WeatherData?.data?.location.name;
+    if (!cityName) return;
+    if (Favorites.includes(cityName)) {
+      removeFromFavorites(cityName);
+      toast.success(`Removed ${cityName} from favorites`);
+    } else {
+      addToFavorites(cityName);
+      toast.success(`Added ${cityName} to favorites`);
+    }
+  };
+
+  const [isFavorite, setIsFavorite] = useState<boolean>(false);
+  useEffect(() => {
+    if (WeatherData?.data?.location.name) {
+      setIsFavorite(Favorites.includes(WeatherData.data.location.name));
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [WeatherData, handleFavorite]);
+
   return (
     <div className="col-start-2 col-span-full bg-black/20 rounded-md p-3 flex items-center gap-3">
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button
+            className={`flex justify-center items-center cursor-pointer text-muted-foreground ${
+              isFavorite ? "!bg-emerald-900 text-white" : ""
+            }`}
+            variant={"outline"}
+            onClick={handleFavorite}
+          >
+            <Sparkle className="bigger-icon" />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>
+          <span className="text-lg">
+            {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+          </span>
+        </TooltipContent>
+      </Tooltip>
       <Input
         placeholder="Search by City"
         className="bg-white"
